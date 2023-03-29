b077ebfb5307c4055ea89273d0086e385c5271b0
maxwell-petre <maxwell.petre73@gmail.com>
2023-03-29 16:48:23 +1300

    Adding Error Handling

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -8,12 +9,12 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : MediatR.IRequest<List<Activity>>
+        public class Query : MediatR.IRequest<Result<List<Activity>>>
         {
         }
 
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
         private readonly DataContext _context;
 
@@ -24,9 +25,9 @@ namespace Application.Activities
             }
 
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken token)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken token)
             {
-                return await _context.Activities.ToListAsync();
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
             }
         }
     }
